3d6444acb39f9afb1c05b26ba21b53f8cc9d3903
OndraKonicek <konicekondra@gmail.com>
2021-11-15 16:21:34 +0100

    all

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@ User's List - Users waiting for an approval.
 <ul>
     @foreach ($users as $user)
         <li>
-            <p>{{ $user->first_name }} {{ $user->last_name }}, {{ $user->email }} </p>
+            <p>{{ $user->first_name }} {{ $user->last_name }}, {{ $user->email }} <button>Accept</button> <button>Denied</button> </p>
         </li>
     @endforeach
 </ul>
