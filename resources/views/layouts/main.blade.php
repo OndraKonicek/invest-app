348ea435afc39bc660ec8db890a4fdc34ab42834
Filip Trbusek <filiptrbusek@gmail.com>
2021-11-18 16:02:27 +0100

    styling on approval page and created date => euro array

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Invest App</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -45,7 +46,7 @@
                 
                 
                 <div class="dropdown">
-                    <button class="dropbtn">{{Auth::user()->first_name}} {{Auth::user()->last_name}} 🔽 
+                    <button class="dropbtn">{{Auth::user()->first_name}} {{Auth::user()->last_name}} 
                     <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
--- resources/views/not-approved.blade.php
+++ resources/views/not-approved.blade.php
@@ -2,7 +2,21 @@
 
 @section('content')
 
-<h1>WAITING FOR APPROVED</h1>
+    <div class="wrapper_approval">
+
+        <section class="approval_section">
+
+            <h1>Hi {{Auth::user()->first_name}}</h1>
+
+            <h2>Your account has been succesfully created</h2> 
+
+            <p>Your account is now awaiting to be approved by an administrator. We will notify by email as soon as your account will be approved.</p>
+
+        </section>
+
+    </div>
+
+
    
 @endsection
 
