a3e3b61fc02a05a565a431912bce55ecbc1e8484
JulieKuncarova <KuncarovaJulie@seznam.cz>
2021-11-18 16:33:02 +0100

    adding invest-now button, deposits page - adding form

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,10 +4,19 @@
 
 
 @can('investor')
-    <h1>Dashboard</h1>
-
-    <div>Name: {{Auth::user()->first_name}} {{Auth::user()->last_name}}</div>
-
+        <h1>Dashboard</h1>
+    
+    
+            <div>Name: {{Auth::user()->first_name}} {{Auth::user()->last_name}}</div>
+    
+            <br>
+        
+        
+            <a href="/deposits"> 
+                <button>Invest now!</button>
+            </a>
+      
+    
     {{-- {{dd($user)}} --}}
 
 @endcan
--- resources/views/deposits/index.blade.php
+++ resources/views/deposits/index.blade.php
@@ -4,6 +4,17 @@
 
     <h1>Deposits</h1>
 
+    <h3>Balanced portfolio</h3>
+
+
+    <form action="" method="post">
+            @csrf
+        <label for="deposit">Amount:</label>
+        <input type="text" id="deposit">
+    
+        <input type="submit">
+    </form>
+
     {{-- @if($user->date_approved == null)
         uzitele musi povolit admin
     @else
